7a13ee79d31ddb263d318516eb2ebe8b69ff8c59
dev-dong <dho0647@naver.com>
2023-03-14 20:16:52 +0900

    signup-v1

---
+++ src/main/java/com/iam/shopping/controller/member/MemberController.java
@@ -0,0 +1,40 @@
+package com.iam.shopping.controller.member;
+
+import com.iam.shopping.domain.member.MemberRepository;
+import com.iam.shopping.domain.member.MemberVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberController(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "member/signup";
+    }
+
+    @PostMapping("/signup")
+    public String saveMember(@RequestParam("id") String id,
+                             @RequestParam("pass") String pass,
+                             @RequestParam("name") String name,
+                             @RequestParam("phone") String phone,
+                             Model model) {
+        MemberVO memberVO = new MemberVO(id, pass, name, phone);
+        memberRepository.save(memberVO);
+        model.addAttribute("member", memberVO);
+
+        return "index";
+    }
+}
---
+++ src/main/java/com/iam/shopping/domain/member/MemberRepository.java
@@ -0,0 +1,23 @@
+package com.iam.shopping.domain.member;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class MemberRepository {
+    private static final Map<Long, MemberVO> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    // 회원 저장
+    public MemberVO save(MemberVO memberVO) {
+        memberVO.setNum(++sequence);
+        store.put(memberVO.getNum(), memberVO);
+        return memberVO;
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
