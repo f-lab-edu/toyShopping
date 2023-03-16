b6727b869a9970f5b50c26f089f7299ba3191adb
dev-dong <dho0647@naver.com>
2023-03-16 13:00:11 +0900

    chagne @GetMapping to @RequestMapping

--- src/main/java/com/iam/shopping/controller/member/MemberController.java
+++ src/main/java/com/iam/shopping/controller/member/MemberController.java
@@ -5,10 +5,7 @@ import com.iam.shopping.domain.member.MemberVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/member")
@@ -20,7 +17,7 @@ public class MemberController {
         this.memberRepository = memberRepository;
     }
 
-    @GetMapping("/signup")
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
     public String signup() {
         return "member/signup";
     }
