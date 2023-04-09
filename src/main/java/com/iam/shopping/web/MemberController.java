aeed1cf10cb40c6124542362104d0f486b279578
dev-dong <dho0647@naver.com>
2023-04-09 18:13:19 +0900

    @RestController

--- src/main/java/com/iam/shopping/web/MemberController.java
+++ src/main/java/com/iam/shopping/web/MemberController.java
@@ -1,11 +1,12 @@
 package com.iam.shopping.web;
 
 import com.iam.shopping.domain.Member;
-import com.iam.shopping.mapper.MemberMapper;
 import com.iam.shopping.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/member")
 @RestController
@@ -13,8 +14,19 @@ public class MemberController {
     @Autowired
     private MemberService memberService;
 
+    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    public Member memberInfo() {
+        Member member = new Member();
+        member.setId("test@naver.com");
+        member.setPass("1234");
+        member.setName("이동호");
+        member.setPhone("010-4819-6169");
+        return member;
+    }
+
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public void memberSignupPOST(Member member) {
+    public void memberSignupPOST(@RequestBody Member member) {
+        System.out.println(member);
         memberService.memberSignup(member);
     }
 }
