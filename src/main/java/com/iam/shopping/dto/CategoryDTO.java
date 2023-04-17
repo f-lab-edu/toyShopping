832b64841ba2258c4957d86ed1e0c56b320c12aa
dev-dong <dho0647@naver.com>
2023-04-17 20:32:44 +0900

    CategoryDTO Lombok use

--- src/main/java/com/iam/shopping/dto/CategoryDTO.java
+++ src/main/java/com/iam/shopping/dto/CategoryDTO.java
@@ -1,32 +1,11 @@
 package com.iam.shopping.dto;
 
+import lombok.Data;
+
+@Data
 public class CategoryDTO {
     private int depth;
     private String categoryName;
     private String parentCategoryId;
     private String parentCategoryName;
-
-    public int getDepth() {
-        return depth;
-    }
-
-    public void setDepth(int depth) {
-        this.depth = depth;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public String getParentCategoryId() {
-        return parentCategoryId;
-    }
-
-    public void setParentCategoryId(String parentCategoryId) {
-        this.parentCategoryId = parentCategoryId;
-    }
-
-    public String getParentCategoryName() {
-        return parentCategoryName;
-    }
 }
