882f6ccced042c5554021b3556dddd2e07d5e8a1
dev-dong <dho0647@naver.com>
2023-04-26 03:02:43 +0900

    item html

---
+++ src/main/java/com/iam/shopping/web/ItemController.java
@@ -0,0 +1,12 @@
+package com.iam.shopping.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+}
